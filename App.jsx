e9dfbaa8fde5e8de4ef1924fd7db0dcf9754cdb0
s4rmier <135184315+s4rmier@users.noreply.github.com>
2023-07-07 19:55:04 -0700

    first commit

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import Header from "./Header";
+import ReactDOM from "react-dom";
+
+export default function App() {
+  return <Header />;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <nav className="flex-row container align">
+        <div className="flex-row align">
+          <img src="./img/meme-logo.png" alt="" />
+          <h1>Meme Generator</h1>
+        </div>
+        <h2>React Course - Project 1</h2>
+      </nav>
+    </header>
+  );
+}
